9b9656fc7217da6277de0249f946e25ba33e6e3f
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2021-11-11 11:52:18 +0700

    fiturpengaduan

--- rumah_sidoarjo/lib/pages/Pengaduan/pengaduan.dart
+++ rumah_sidoarjo/lib/pages/Pengaduan/pengaduan.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rumah_sidoarjo/custom_template.dart';
 import 'package:rumah_sidoarjo/home.dart';
+import 'package:rumah_sidoarjo/pages/Pengaduan/detailpengaduan.dart';
 
 class Pengaduan extends StatefulWidget {
   static String routeName = "/pengaduan";
@@ -16,28 +18,108 @@ class _PengaduanState extends State<Pengaduan> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey.shade300,
         appBar: appBar(),
-        body: Scaffold(
-          backgroundColor: White,
-          body: AnnotatedRegion<SystemUiOverlayStyle>(
-            value: SystemUiOverlayStyle.light,
-            child: GestureDetector(
-              onTap: () => FocusScope.of(context).unfocus(),
-              child: Stack(
-                children: <Widget>[
-                  SingleChildScrollView(
-                    physics: AlwaysScrollableScrollPhysics(),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 40.0,
-                      vertical: 120.0,
-                    ),
-                  ),
-                  _headerPage(),
-                ],
+        body: Column(
+          children: [
+            _headerPage(),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: kategori.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 2),
+                        child: Container(
+                          height: 85,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    children: [
+                                      Container(
+                                        width: 300,
+                                        height: 55,
+                                        child: Center(
+                                          child: Column(
+                                            children: [
+                                              Text(kategori[index],
+                                                  style: GoogleFonts.poppins(
+                                                    fontSize: 12,
+                                                    fontWeight: FontWeight.bold,
+                                                  )),
+                                              Text(subKategori[index],
+                                                  textAlign: TextAlign.center,
+                                                  style: GoogleFonts.dmSans(
+                                                    fontSize: 12,
+                                                  ))
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 10),
+                                    child: IconButton(
+                                        onPressed: () {
+                                          Navigator.pushReplacement(context,
+                                              MaterialPageRoute(
+                                                  builder: (context) {
+                                            return DetailPengaduan();
+                                          }));
+                                        },
+                                        icon: Icon(
+                                          Icons.arrow_forward_ios,
+                                          size: 25,
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _headerPage() {
+    return Container(
+      height: 158,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [lightGreen, darkGreen1],
+              end: Alignment.centerRight,
+              begin: Alignment.centerLeft),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30))),
+      child: Column(
+        children: [
+          SizedBox(height: 10),
+          Text(
+            'Pengaduan',
+            style: headerTextStyle,
+          ),
+          Center(
+            child: Container(
+              width: 330,
+              child: Text(
+                'Ayo adukan kerusakan fasilitas publik di lingkungan Anda agar segera mendapatkan penanganan dari dinas terkait',
+                textAlign: TextAlign.justify,
+                style: headerSubTextStyle,
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -65,41 +147,16 @@ class _PengaduanState extends State<Pengaduan> {
       backgroundColor: darkGreen1,
     );
   }
-
-  Widget _headerPage() {
-    return Expanded(
-      child: Container(
-        height: 158,
-        width: double.infinity,
-        decoration: headerDecoration,
-        child: Stack(
-          children: <Widget>[
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 10),
-                  ),
-                  Text(
-                    'Pengaduan',
-                    style: headerTextStyle,
-                  ),
-                  Container(
-                    width: 350,
-                    child: Center(
-                      child: Text(
-                        'Ayo adukan kerusakan fasilitas publik di lingkungan Anda agar segera mendapatkan penanganan dari dinas terkait',
-                        textAlign: TextAlign.justify,
-                        style: headerSubTextStyle,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
+
+final List kategori = [
+  "Pelayanan",
+  "Fasilitas Publik",
+  "Kesehatan",
+];
+
+final List subKategori = [
+  "Kependudukan, Perizinan, Pajak Daerah, PDAM, dll",
+  "Infrastruktur, Pasar, Rusunawa, Penerangan Jalan, CCTV, dll",
+  "Gizi Buruk/Stunting, Fasilitas Kesehatan, Mal Praktik Kesehatan, dll",
+];
