02a7d40a8db3ff7acd660cec707e73dd113f7977
MHanafiAbror87 <E41180749@student.polije.ac.id>
2021-11-30 13:34:44 +0700

    UI Kesehatan

--- rumah_sidoarjo/lib/pages/kesehatan/kesehatan.dart
+++ rumah_sidoarjo/lib/pages/kesehatan/kesehatan.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rumah_sidoarjo/custom_template.dart';
 import 'package:rumah_sidoarjo/home.dart';
+import 'package:rumah_sidoarjo/pages/pariwisata/pemancingan.dart';
 
 class Kesehatan extends StatefulWidget {
   Kesehatan({Key? key}) : super(key: key);
@@ -17,10 +19,21 @@ class _KesehatanState extends State<Kesehatan> {
       home: Scaffold(
         backgroundColor: Colors.grey.shade300,
         appBar: appBar(),
-        body: Column(
-          children: [
-            _headerPage(),
-          ],
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Stack(
+            children: <Widget>[
+              SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40.0,
+                  // vertical: 120.0,
+                ),
+              ),
+              _headerPage(),
+              tabCon(),
+            ],
+          ),
         ),
       ),
     );
@@ -74,6 +87,63 @@ class _KesehatanState extends State<Kesehatan> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class tabCon extends StatelessWidget {
+  const tabCon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 170.0, left: 10, right: 10),
+        child: DefaultTabController(
+          length: 3,
+          child: Column(
+            children: [
+              TabBar(
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicatorColor: darkGreen1,
+                  indicator: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [lightGreen, darkGreen1],
+                          end: Alignment.centerRight,
+                          begin: Alignment.centerLeft),
+                      borderRadius: BorderRadius.circular(30)),
+                  labelColor: White,
+                  labelStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                  unselectedLabelColor: darkGrey,
+                  unselectedLabelStyle: TextStyle(fontSize: 14),
+                  tabs: [
+                    Tab(child: Text('PKM Utama')),
+                    Tab(
+                        child: Text(
+                      'PKM Pembantu',
+                      textAlign: TextAlign.center,
+                    )),
+                    Tab(child: Text('Rumah Sakit')),
+                  ]),
+              SizedBox(height: 10),
+              Container(
+                height: 595,
+                width: double.infinity,
+                child: TabBarView(
+                  children: [
+                    Pemancingan(),
+                    Pemancingan(),
+                    Pemancingan(),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
