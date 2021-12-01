a57e168c9633c3b23bf1da8f73444f439d447f34
harismansihombing <sihombingharisman28@gmail.com>
2021-12-01 09:57:26 +0700

    Detail Umkm
    Detail umkm

--- rumah_sidoarjo/lib/pages/detail_umkm.dart
+++ rumah_sidoarjo/lib/pages/detail_umkm.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rumah_sidoarjo/custom_template.dart';
 import 'package:rumah_sidoarjo/home.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:getwidget/getwidget.dart';
 
 class Detail_Umkm extends StatefulWidget {
   @override
@@ -26,204 +27,220 @@ class _Detail_UmkmState extends State<Detail_Umkm> {
               return Home();
             })),
           ),
-          title: Text('Detail UMKM'),
+          title: Text('Detail Umkm'),
           backgroundColor: darkGreen1,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 10),
-              child: Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/no_image.png')),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage('assets/images/no_image.png')),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              'Disini Tempat nama UMKM',
-              style: TextStyle(
-                  fontFamily: 'DMSans',
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Kategori',
-                      style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Kerajinan',
-                      style: TextStyle(
-                          fontFamily: 'DMSans', fontSize: 14, color: darkGreen),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Alamat',
-                      style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Disini Alamat UMKM',
-                      style: TextStyle(
-                          fontFamily: 'DMSans', fontSize: 14, color: darkGreen),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                  Row(
+              Text(
+                'Nama Umkm',
+                style: TextStyle(
+                    fontFamily: 'DMSans',
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      Column(
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Alamat',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Jl. Diponegoro No. 139 Sidoarjo',
+                          style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontSize: 14,
+                              color: darkGreen),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Kategori',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Pemancingan',
+                          style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontSize: 14,
+                              color: darkGreen),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Pengelola',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Harisman Sihombing',
+                          style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontSize: 14,
+                              color: darkGreen),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      Row(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 27),
-                            child: Align(
-                              alignment: Alignment.bottomLeft,
-                              child: Text(
-                                'Telepon',
-                                style: TextStyle(
-                                  fontFamily: 'DMSans',
-                                  fontSize: 16,
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 27),
+                                child: Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Text(
+                                    'Telepon',
+                                    style: TextStyle(
+                                      fontFamily: 'DMSans',
+                                      fontSize: 16,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  '031-8941104',
+                                  style: TextStyle(
+                                      fontFamily: 'DMSans',
+                                      fontSize: 14,
+                                      color: darkGreen),
+                                ),
+                              ),
+                            ],
                           ),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '031-8941104',
-                              style: TextStyle(
-                                  fontFamily: 'DMSans',
-                                  fontSize: 14,
-                                  color: darkGreen),
-                            ),
-                          ),
+                          SizedBox(width: 200),
+                          Container(
+                              width: 100,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: darkGreen1,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Center(
+                                  child: Text(
+                                'Panggil',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 14, color: White),
+                              ))),
                         ],
                       ),
-                      SizedBox(width: 200),
-                      Container(
-                          width: 100,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: darkGreen1,
-                            borderRadius: BorderRadius.circular(30),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Deskripsi',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16,
                           ),
-                          child: Center(
-                              child: Text(
-                            'Panggil',
-                            style:
-                                GoogleFonts.poppins(fontSize: 14, color: White),
-                          ))),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Harisman Sihombing, Harisman Sihombing, Harisman Sihombing, Harisman Sihombing, Harisman Sihombing, Harisman Sihombing,Harisman Sihombing',
+                          style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontSize: 14,
+                              color: darkGreen),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Media Sosial',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Harisman Sihombing',
+                          style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontSize: 14,
+                              color: darkGreen),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        height: 25,
+                        thickness: 2,
+                      ),
+                      SizedBox(height: 20),
                     ],
                   ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Website/Media Sosial',
-                      style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Disini bisa link webiste bisa link Media Sosial',
-                      style: TextStyle(
-                          fontFamily: 'DMSans', fontSize: 14, color: darkGreen),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Deskripsi',
-                      style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Disini berisi detail dari umkm.',
-                      style: TextStyle(
-                          fontFamily: 'DMSans', fontSize: 14, color: darkGreen),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Rute Toko',
-                      style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Link Mengarah ke google Maps',
-                      style: TextStyle(
-                          fontFamily: 'DMSans', fontSize: 14, color: darkGreen),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    height: 25,
-                    thickness: 2,
-                  ),
-                ],
-              ),
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
